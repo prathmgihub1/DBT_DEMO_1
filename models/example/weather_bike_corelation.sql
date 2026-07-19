e562297fd311b5fb7e44aefce5cc35666cec601b
Priya U <prathmrelekar@gmail.com>
2026-07-19 19:04:51 +0000

    correction of corelated model

--- models/example/weather_bike_corelation.sql
+++ models/example/weather_bike_corelation.sql
@@ -1,6 +1,6 @@
 with cte as (
 select * from {{ ref('weather_dimension') }} w
-left join {{ ref('trip_fact') }} t on w.DAILY_WEATHER=t.DAILY_WEATHER
+inner join {{ ref('trip_fact') }} t on w.DAILY_WEATHER=t.DAILY_WEATHER
 
 )
 
